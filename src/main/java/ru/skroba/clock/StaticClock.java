1719ade2c08a7ad3096c8ea38dea1b17057c3175
”Dmitri <dmitri.zd1958@gmail.com>
2022-11-11 17:29:11 +0100

    Feature: add sability set time

--- src/main/java/ru/skroba/clock/StaticClock.java
+++ src/main/java/ru/skroba/clock/StaticClock.java
@@ -3,9 +3,16 @@ package ru.skroba.clock;
 import java.time.Instant;
 
 public final class StaticClock implements Clock {
-    private final Instant time;
+    private Instant time;
     
     public StaticClock(final Instant time) {
+        this.time = time;
+    }
+    
+    public void setTime(final Instant time) {
+        if (time == null) {
+            throw new IllegalArgumentException("Instant can't be null!");
+        }
         this.time = time;
     }
     
